e0fa46de8aef76d976ec92e3a788f0df8320f4ad
Yashika <dhawral.yashika@gmail.com>
2026-08-21 02:41:48 +0530

    fixed

--- src/test/java/com/vit/demo/AppTest.java
+++ src/test/java/com/vit/demo/AppTest.java
@@ -3,7 +3,7 @@ package com.vit.demo;
 public class AppTest {
 
     public static void main(String[] args) {
-        int expected = 6;
+        int expected = 5;
         int actual = App.add(2, 3);
 
         if (actual != expected) {
